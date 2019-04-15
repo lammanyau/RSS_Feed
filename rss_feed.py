cb8e313b23299f0b2e4d5e0a83d2ce72ac74747d
lammanyau <49655700+lammanyau@users.noreply.github.com>
2019-04-15 19:15:15 -0400

    Delete rss_feed.py

--- rss_feed.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-
-
-def get_posts(name):
-    posts = []
-    url = 'https://9gag-rss.com/api/rss/get?code=9GAG' + name + '&format=1'
-    posts.extend(feedparser.parse(url).entries)
-    return posts
-
